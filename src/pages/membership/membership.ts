faa95cc4dec78b29d1ba9c550acf07a024f813c8
Kritsana <tragoolphrom@gmail.com>
2018-07-24 16:35:18 +0700

    Demo Lotto
    Signed-off-by: Kritsana <tragoolphrom@gmail.com>

---
+++ src/pages/membership/membership.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TradePage } from '../trade/trade';
+import { CollectionPage } from '../collection/collection';
+import { TicketPage } from '../ticket/ticket';
+
+/**
+ * Generated class for the MembershipPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-membership',
+  templateUrl: 'membership.html',
+})
+export class MembershipPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MembershipPage');
+  }
+
+  Trade(){
+    this.navCtrl.push(TradePage);
+  }
+
+  Collection(){
+    this.navCtrl.push(CollectionPage);
+  }
+
+  Ticket(){
+    this.navCtrl.push(TicketPage);
+  }
+}
