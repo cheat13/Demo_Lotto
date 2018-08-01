b47f623fc19bbe3c8592fa06818073df5542cf9e
Kritsana <tragoolphrom@gmail.com>
2018-08-01 10:46:04 +0700

    App compo
    Signed-off-by: Kritsana <tragoolphrom@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { MembershipPage } from '../pages/membership/membership';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = MembershipPage;
+  rootPage:any = Web1Page;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
