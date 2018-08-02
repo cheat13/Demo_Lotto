934b21e58f114c68e2a5a8b8cdb04fb0ba23ee8f
kneww <thanachart_d@kkumail.com>
2018-08-02 13:33:02 +0700

    added game CCC

---
+++ src/pages/convertclick-c/convertclick-c.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ConvertclickCPage } from './convertclick-c';
+
+@NgModule({
+  declarations: [
+    ConvertclickCPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ConvertclickCPage),
+  ],
+})
+export class ConvertclickCPageModule {}
