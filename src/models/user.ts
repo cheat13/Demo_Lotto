d6c20a071afebd5650e67c9d00a7dd401ccc34ee
sinthuC <sinthu_ch@kkumail.com>
2018-08-03 14:21:51 +0700

    UPDATE model

---
+++ src/models/user.ts
@@ -0,0 +1,19 @@
+import { ScratchLotto } from '../models/scratchLotto';
+import { SlotMachine } from '../models/slotMachine';
+
+export class User{
+        coins:number; 
+        ethereum:number;
+        money:number;
+        scratchGame :ScratchLotto;
+        slotGame:SlotMachine;
+
+        constructor() {
+            this.coins = 0;
+            this.ethereum = 0;
+            this.money = 50000;
+            this.scratchGame = new ScratchLotto();
+            this.slotGame = new SlotMachine();
+            
+        }
+    }
