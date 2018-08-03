2e68705dfbe289d37ae88c2605ca5148ca4d4e42
Jormthup <jomtup12@gmail.com>
2018-08-03 17:08:58 +0700

    add scratch game

--- src/pages/lottogame/lottogame.ts
+++ src/pages/lottogame/lottogame.ts
@@ -18,6 +18,15 @@ import { PrePlaygamePage } from '../pre-playgame/pre-playgame';
   templateUrl: 'lottogame.html',
 })
 export class LottogamePage {
+  img1 = '../../assets/imgs/cirpink.png';
+  img2 = '../../assets/imgs/cirpink.png';
+  img3 = '../../assets/imgs/cirpink.png';
+  img4 = '../../assets/imgs/cirpink.png';
+  img5 = '../../assets/imgs/cirpink.png';
+  img6 = '../../assets/imgs/cirpink.png';
+  img7 = '../../assets/imgs/cirpink.png';
+  img8 = '../../assets/imgs/cirpink.png';
+  img9 = '../../assets/imgs/cirpink.png';
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
@@ -56,4 +65,58 @@ export class LottogamePage {
     });
     alert.present();
 }
+  addEvent1(e){
+    if(this.img1=='../../assets/imgs/cirpink.png')
+    {
+      this.img1 = '../../assets/imgs/phodrum.png'
+    }
+  }
+  addEvent2(e){
+    if(this.img2=='../../assets/imgs/cirpink.png')
+    {
+      this.img2 = '../../assets/imgs/phodrum.png'
+    }
+  }
+  addEvent3(e){
+    if(this.img3=='../../assets/imgs/cirpink.png')
+    {
+      this.img3 = '../../assets/imgs/phodrum.png'
+    }
+  }
+  addEvent4(e){
+    if(this.img4=='../../assets/imgs/cirpink.png')
+    {
+      this.img4 = '../../assets/imgs/phodrum.png'
+    }
+  }
+  addEvent5(e){
+    if(this.img5=='../../assets/imgs/cirpink.png')
+    {
+      this.img5 = '../../assets/imgs/phodrum.png'
+    }
+  }
+  addEvent6(e){
+    if(this.img6=='../../assets/imgs/cirpink.png')
+    {
+      this.img6 = '../../assets/imgs/phodrum.png'
+    }
+  }
+  addEvent7(e){
+    if(this.img7=='../../assets/imgs/cirpink.png')
+    {
+      this.img7 = '../../assets/imgs/phodrum.png'
+    }
+  }
+  addEvent8(e){
+    if(this.img8=='../../assets/imgs/cirpink.png')
+    {
+      this.img8 = '../../assets/imgs/phodrum.png'
+    }
+  }
+  addEvent9(e){
+    if(this.img9=='../../assets/imgs/cirpink.png')
+    {
+      this.img9 = '../../assets/imgs/phodrum.png'
+    }
+  }
 }
